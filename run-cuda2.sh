2df0518dd4947b140c89857a60ce09a9a5db14d1
Alexandre Ichida <yukioichida@gmail.com>
2019-06-12 10:19:47 -0300

    include scripts per cuda device #3

---
+++ run-cuda2.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+export CUDA_VISIBLE_DEVICES=2
+
+n_heads=1
+n_blocks=1
+word_dim=300
+
+max_seq_size=150
+
+python3 run_train.py --n_heads=$n_heads --n_blocks=$n_blocks --word_dim=$word_dim --max_seq_size=$max_seq_size
